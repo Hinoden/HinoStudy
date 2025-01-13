08588e177e76831195c08c9e55e92cb755d653bf
Hinoden <krwong3@gmail.com>
2025-01-12 17:49:48 -0800

    Incorporated a skip button. Break and work messages integrated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Popup from 'reactjs-popup';
 import '../styles/Navbar.css';
 
 function Navbar() {
@@ -7,10 +8,58 @@ function Navbar() {
             <p id="logo">HinoStudy</p>
             <div className="ButContainer">
                 <button id="brainrot">Brainrot</button>
-                <button id="settings">Settings</button>
+                <Popup trigger = 
+                    {<button id="settings">Settings</button>}
+                    modal nested>
+                    {
+                        close => (
+                            <div className='PopupContainer'>
+                                <div className='PopupTitle'>
+                                    Settings
+                                </div>
+                                <div>
+                                    <button onClick = {() => close()}>Close</button>
+                                </div>
+                            </div>
+                        )
+                    }
+                    </Popup>
             </div>
         </div>
     );
 }
 
 export default Navbar;
+
+// Filename: App.js
+
+// import React from 'react';
+// import Popup from 'reactjs-popup';
+// import 'reactjs-popup/dist/index.css';
+
+// export default function PopupGfg() {
+//     return (
+//         <div>
+//             <h4>Popup - GeeksforGeeks</h4>
+//             <Popup trigger=
+//                 {<button> Click to open modal </button>} 
+//                 modal nested>
+//                 {
+//                     close => (
+//                         <div className='modal'>
+//                             <div className='content'>
+//                                 Welcome to GFG!!!
+//                             </div>
+//                             <div>
+//                                 <button onClick=
+//                                     {() => close()}>
+//                                         Close modal
+//                                 </button>
+//                             </div>
+//                         </div>
+//                     )
+//                 }
+//             </Popup>
+//         </div>
+//     )
+// };
--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -1,104 +1,121 @@
-import React, {useState, useEffect} from 'react';
-import {BsStopwatch} from 'react-icons/bs';
-import {BsFillPlayFill, BsPauseFill, BsStopFill} from "react-icons/bs"
+import React, { useState, useEffect } from 'react';
+import { BsStopwatch } from 'react-icons/bs';
+import { BsFillPlayFill, BsPauseFill, BsStopFill } from "react-icons/bs";
+import { IoIosSkipForward } from "react-icons/io";
 import '../styles/Timer.css';
 
 function Timer() {
     const [hours, setHours] = useState(0);
     const [minutes, setMinutes] = useState(0);
     const [seconds, setSeconds] = useState(0);
-    const [isRunning, setIsRunning] = useState(null);
-    const [showEndScreen, setShowEndScreen] = useState({
-        show: false,
-        message: "Time's up! Let's take a break!"
-    })
+    const [isRunning, setIsRunning] = useState(false);
+    const [isBreak, setIsBreak] = useState(false);
+    const [currentScreen, setCurrentScreen] = useState("");
 
     useEffect(() => {
         let interval;
-        if (isRunning){
+
+        if (isRunning) {
             interval = setInterval(() => {
-                if (seconds > 0){
-                    setSeconds((seconds) => seconds - 1);
-                } else if (minutes > 0){
-                    setMinutes((minutes) => minutes - 1);
+                if (seconds > 0) {
+                    setSeconds((prev) => prev - 1);
+                } else if (minutes > 0) {
+                    setMinutes((prev) => prev - 1);
                     setSeconds(59);
-                } else if (hours > 0){
-                    setHours((hours) => hours - 1);
+                } else if (hours > 0) {
+                    setHours((prev) => prev - 1);
                     setMinutes(59);
                     setSeconds(59);
+                } else {
+                    handleTimerEnd();
                 }
             }, 1000);
         }
 
-        if (hours === 0 && minutes === 0 && seconds === 1){
-            setShowEndScreen({...showEndScreen, show: true});
-        }
-        if (hours === 0 && minutes === 0 && seconds === 0){
-            resetTimer();
-        }
         return () => clearInterval(interval);
-    }, [seconds, minutes, hours, isRunning, showEndScreen.show]);
+    }, [hours, minutes, seconds, isRunning]);
 
-    function startTimer(){
-        if (hours !==0 || minutes !==0 || seconds !==0){
+    function handleTimerEnd() {
+        setIsRunning(false);
+
+        if (isBreak) {
+            setCurrentScreen("Time's up! Let's get back to working!");
+        } else {
+            setCurrentScreen("Time's up! Let's take a break!");
+        }
+
+        setIsBreak(!isBreak);
+    }
+
+    function startTimer() {
+        if (hours !== 0 || minutes !== 0 || seconds !== 0) {
             setIsRunning(true);
-            setShowEndScreen({...showEndScreen, show: false});
-        }
-        else{
-            window.alert("You forgot to add time, silly<3")
+            setCurrentScreen("");
+        } else {
+            window.alert("You forgot to add time, silly<3");
         }
     }
 
-    function pauseTimer(){
+    function pauseTimer() {
         setIsRunning(false);
     }
 
-    function stopTimer(){
+    function stopTimer() {
         resetTimer();
-        setShowEndScreen({...showEndScreen, show: false});
+        setCurrentScreen("");
     }
 
-    function resetTimer(){
+    function resetTimer() {
         setIsRunning(false);
-        setSeconds(0);
-        setMinutes(0);
         setHours(0);
+        setMinutes(0);
+        setSeconds(0);
+    }
+
+    function skipTimer() {
+        setIsBreak(!isBreak);
+        if (isBreak) {
+            setCurrentScreen("Time's up! Let's get back to working!");
+        } else {
+            setCurrentScreen("Time's up! Let's take a break!");
+        }
+        resetTimer();
     }
 
     const changeSeconds = (e) => {
-        setSeconds(e.target.value)
-    }
+        setSeconds(parseInt(e.target.value) || 0);
+    };
 
     const changeMinutes = (e) => {
-        setMinutes(e.target.value)
-    }
+        setMinutes(parseInt(e.target.value) || 0);
+    };
 
     const changeHours = (e) => {
-        setHours(e.target.value)
-    }
+        setHours(parseInt(e.target.value) || 0);
+    };
 
     return (
         <div className="TimerContainer">
             <p id="TimerTitle">Pomodoro Timer</p>
-            {showEndScreen.show && <p id="message">{showEndScreen.message}</p>}
-                <div className="TimerWrapper">
-                    <BsStopwatch className="stop-watch"/>
-                    <div className="column">
-                        <label>Hours</label>
-                        <input value={hours} onChange={changeHours}/>
-                    </div>
-                    <div className="column">
-                        <label>Minutes</label>
-                        <input value={minutes} onChange={changeMinutes}/>
-                    </div>{" "}
-                    <div className="column">
-                        <label>Seconds</label>
-                        <input value={seconds} onChange={changeSeconds}/>
-                    </div>
+            {currentScreen && <p id="message">{currentScreen}</p>}
+            <div className="TimerWrapper">
+                <BsStopwatch className="stop-watch" />
+                <div className="column">
+                    <label>Hours</label>
+                    <input value={hours} onChange={changeHours} />
                 </div>
+                <div className="column">
+                    <label>Minutes</label>
+                    <input value={minutes} onChange={changeMinutes} />
+                </div>
+                <div className="column">
+                    <label>Seconds</label>
+                    <input value={seconds} onChange={changeSeconds} />
+                </div>
+            </div>
 
-                <br />
-            
+            <br />
+
             <div className="buttonContainer">
                 {!isRunning && (
                     <button className="accept-button" onClick={startTimer}>
@@ -112,6 +129,9 @@ function Timer() {
                 )}
                 <button className="danger-button" onClick={stopTimer}>
                     <BsStopFill />
+                </button>
+                <button className="skip-button" onClick={skipTimer}>
+                    <IoIosSkipForward />
                 </button>
             </div>
         </div>
