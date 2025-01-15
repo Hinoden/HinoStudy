8c7b692ca25622be8843e9bb747f45c03af95082
Hinoden <krwong3@gmail.com>
2025-01-14 16:39:28 -0800

    Replaced brainrot video with a better one.

--- src/components/Brainrot.jsx
+++ src/components/Brainrot.jsx
@@ -4,7 +4,7 @@ import '../styles/Brainrot.css';
 function Brainrot() {
     return (
         <div className="BrainrotContainer">
-            <iframe width="448" height="252" src="https://www.youtube.com/embed/5MQ1sq3VEic?si=qvR_wbE5E94Aah5m&amp;start=18" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="448" height="252" src="https://www.youtube.com/embed/L_fcrOyoWZ8?si=Fr5sSgoK-Enf7tW3" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
             <iframe width="448" height="252" src="https://www.youtube.com/embed/REuKymvrrqk?si=9_CifzWekrbkAEMQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </div>
     )
