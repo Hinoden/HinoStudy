636222b6cf0b9ad8a6bfc708c19340efa84bf401
Hinoden <krwong3@gmail.com>
2025-08-27 17:06:23 -0700

    Started login/signup components

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+import SignUp from './components/SignUp.jsx';
+import Login from './components/Login.jsx';
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <SignUp />
+      <Login />
+    </div>
+  )
+}
+
+export default App
